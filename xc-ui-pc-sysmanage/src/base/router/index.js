5d583c73515a24566fe3c0ab2792f431a03486e1
kangyong <kangyong_wts@sina.com>
2021-11-08 23:03:06 +0800

    新增表格

--- xc-ui-pc-sysmanage/src/base/router/index.js
+++ xc-ui-pc-sysmanage/src/base/router/index.js
@@ -8,6 +8,8 @@ let concat = (router) => {
 }
 // // 导入路由规则
 import HomeRouter from '@/module/home/router'
+import CMSRouter from '@/module/cms/router'
 // 合并路由规则
 concat(HomeRouter)
+concat(CMSRouter)
 export default routes;

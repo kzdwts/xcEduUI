c83a9914d4e3c64e833add0b2243b19ec8e71b9d
kangyong <kangyong_wts@sina.com>
2022-01-11 22:10:35 +0800

    路由嵌套测试

--- xc-ui-pc-portal/pages/user.vue
+++ xc-ui-pc-portal/pages/user.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>用户管理导航页面</div>
+  <div>
+    用户管理导航页面
+    <nuxt-link :to="'/user/one'">one 页面</nuxt-link>
+    <nuxt-link :to="'/user/102'">修改页面</nuxt-link>
+    <nuxt-child/>
+  </div>
+
 </template>
 
 <script>
---
+++ xc-ui-pc-portal/pages/user/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    修改用户信息{{ id }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "_id",
+  layout: "test",
+  data() {
+    return {
+      id: ''
+    }
+  },
+  mounted() {
+    this.id = this.$route.params.id
+    console.log(this.id)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
